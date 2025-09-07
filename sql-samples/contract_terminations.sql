dedbdda225655731d36776a2e7c577d8459bed5f
kaviyap-git <kaviyapodamekala@gmail.com>
2025-09-07 12:58:13 -0400

    Create contract_terminations.sql

---
+++ sql-samples/contract_terminations.sql
@@ -0,0 +1,11 @@
+-- Pseudo - Code Sample Queries SQL
+
+-- Helped retrive locations where contracts were terminated 
+
+SELECT 
+    location,
+    COUNT(*) AS terminated_contract
+FROM contracts
+WHERE status = 'Terminated'
+GROUP BY location
+ORDER BY terminated_contract DESC;
