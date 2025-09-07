a7c405e2334e6cdb33dd2d19366bac6d39ef3947
kaviyap-git <kaviyapodamekala@gmail.com>
2025-09-07 13:20:16 -0400

    Create pending_service_type.sql

---
+++ sql-samples/pending_service_type.sql
@@ -0,0 +1,10 @@
+-- Pseudo Code SQL Sample Data
+-- Returns the number of work orders that are pending, grouped by the type of service 
+ 
+SELECT 
+  line_of_business,                
+    COUNT(*) AS pending_count  
+FROM invoices
+WHERE status <> 'Completed'      
+GROUP BY line_of_business
+ORDER BY pending_count DESC;    
